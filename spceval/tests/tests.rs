9cee824c3d5c2b931351fbf8eceb177fb133d8d8
Teknomancer <v.ramshankar@gmail.com>
2020-05-31 22:16:23 +0530

    spceval/tests: Todo/comment on what to do.

--- spceval/tests/tests.rs
+++ spceval/tests/tests.rs
@@ -3,14 +3,20 @@ use spceval::{self, Number};
 #[test]
 fn valid_expressions() {
     let expr_results = vec![
+        // Todo: Don't bother testing different radices here.
+        // Those are already covered by unit tests.
+        // Here we should focus on expression constructs rather than validating numbers.
+        // Focus on testing:
+        //    - Operator preceedence.
+        //    - Paranthesis, sub-paranthesis and priority.
+        //    - Functions.
+        //    - Maybe whitespace in odd places.
+        // Make each expression test meaningful and try not to have redundant expressions.
         ("2+2", Number{ integer: 4, float: 4.0 }),
         ("(2+2)", Number{ integer: 4, float: 4.0 }),
         ("1+2*3", Number{ integer: 7, float: 7.0 }),
         ("(1+2)*3", Number{ integer: 9, float: 9.0 }),
         ("(1+2)*(5-1)", Number{ integer: 12, float: 12.0 }),
-        ("0xe + 1", Number{ integer: 0xf, float: 15.0 }),
-        ("0n100 + 4", Number{ integer: 8, float: 8.0 }),
-        ("0o11 + 1", Number{ integer: 10, float: 10.0 }),
         ("0xf << 1", Number{ integer: 0x1e, float: 30.0 }),
         ("((0x128)) + 0n111", Number{ integer: 303, float: 303.0 }),
         ("1*4+(0n1+0xf)", Number{ integer: 20, float: 20.0 }),
