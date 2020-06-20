5c2a541073216ab8a88d941d4cf9785c40452b61
Teknomancer <1089717+Teknomancer@users.noreply.github.com>
2020-06-20 12:10:10 +0530

    src: Start work on bitset descriptions.

---
+++ src/bitset.rs
@@ -0,0 +1,55 @@
+﻿use std::ops::Range;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ByteOrder {
+    LittleEndian,
+    BigEndian,
+}
+
+pub struct BitSetDescription {
+    spans: Range<u8>,
+    name: String,
+    short: String,
+    long: String,
+    kind: BitSetKind,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BitSetReserved {
+    MustBeZero,
+    MustBeOne,
+    Undefined,
+    Ignored,
+}
+
+pub enum BitSetKind {
+    Normal,
+    Rsvd(BitSetReserved),
+}
+
+pub struct BitSet {
+    name: String,
+    arch: String,
+    device: String,
+    byte_order: ByteOrder,
+    bit_count: u8,
+    chunks: Vec<u8>,
+    rsvd: BitSetReserved,
+    show_rsvd: bool,
+    desc: Vec<BitSetDescription>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BitSetError {
+    MissingName,
+    MissingArch,
+    MissingDevice,
+    UnknownArch,
+    InvalidBitCount,
+    InvalidChunkIndex,
+}
+
+pub fn format_bitset(bits: &BitSet) -> Result<String, BitSetError> {
+    Err(BitSetError::MissingName)
+}
+
