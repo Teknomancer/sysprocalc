81accf1d2a4896cbf6bff07184d65d675cd1e020
Teknomancer <1089717+Teknomancer@users.noreply.github.com>
2022-11-02 10:48:42 +0530

    spcregs: register type and conversion.

--- spcregs/src/lib.rs
+++ spcregs/src/lib.rs
@@ -4,8 +4,8 @@ mod register;
 pub mod utils;
 
 pub use bit_range::{BitRange, BitRangeKind, ByteOrder };
-pub use register_descriptor::{RegisterDescriptor, RegisterDescriptorError, MAX_BIT_COUNT };
-pub use register::Register;
+pub use register_descriptor::{RegisterDescriptor, RegisterDescriptorError };
+pub use register::{Register, MAX_BIT_COUNT};
 
 #[cfg(test)]
 mod unit_tests;
