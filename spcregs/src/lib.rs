70849d81f3aec13e4ff6f0cc6fff270404c03493
Teknomancer <1089717+Teknomancer@users.noreply.github.com>
2024-01-05 18:18:12 +0530

    Update to new rustc version and relevant changes.

--- spcregs/src/lib.rs
+++ spcregs/src/lib.rs
@@ -7,6 +7,10 @@ pub use bit_range::{BitRange, BitRangeKind, ByteOrder};
 pub use register_descriptor::{RegisterDescriptor, RegisterDescriptorError};
 pub use register::{Register, MAX_BIT_COUNT};
 
+// Re-export externals
+pub use funty::Unsigned;
+pub use bitvec::mem::BitMemory;
+
 #[cfg(test)]
 mod unit_tests;
 
