b9068b99d246f34f4093048c2e12eb3bb2035223
Teknomancer <v.ramshankar@gmail.com>
2025-08-28 23:30:37 +0530

    spcregs: HashMap of Register. Probably overcomplicating things but too tired now to think straight.

--- spcregs/src/registers.rs
+++ spcregs/src/registers.rs
@@ -1,17 +1,18 @@
 use crate::RegisterDescriptor;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
+use std::sync::LazyLock;
 
 pub mod cpu_x86_registers;
 pub use cpu_x86_registers::X86_CPU_EFER;
 
 pub struct Registers<'a> {
-    registers: BTreeMap<String, &'a RegisterDescriptor>,
+    registers: HashMap<String, &'a RegisterDescriptor>,
 }
 
 impl<'a> Registers<'a> {
     pub fn new() -> Self {
         Self {
-            registers: BTreeMap::new()
+            registers: HashMap::new()
         }
     }
 
@@ -19,4 +20,10 @@ impl<'a> Registers<'a> {
         self.registers.insert(name.to_string(), reg)
     }
 }
+
+pub static REGISTERS: LazyLock<Registers> = LazyLock::new(|| {
+    let mut map: Registers = Registers::new();
+    map.insert(X86_CPU_EFER.name(), &X86_CPU_EFER);
+    map
+});
 
